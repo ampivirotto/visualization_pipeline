154c241b7a3cfb2187efb06bca6cabc5c7264c48
tuh00621 <68647867+tuh00621@users.noreply.github.com>
2021-02-24 18:31:18 -0500

    Create tree_maker.R

---
+++ command_line/tree_maker.R
@@ -0,0 +1,25 @@
+list.of.packages <- c("tidyverse")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, ,repos = "http://cran.us.r-project.org")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager",repos = "http://cran.us.r-project.org")
+BiocManager::install(version = "3.12")
+
+is.installed <- function(mypkg){
+    is.element(mypkg, installed.packages()[,1])
+  } 
+
+  # check if package "ggtree" is installed
+  if (!is.installed("ggtree")){
+    BiocManager::install("ggtree")
+  }
+
+library(tidyverse)
+library(ggtree)
+
+args = commandArgs(trailingOnly=TRUE)
+
+png("tree.png")
+tree <- read.tree(args[1])
+labeled_tree <- ggtree(tree) + geom_tiplab()
+dev.off()
