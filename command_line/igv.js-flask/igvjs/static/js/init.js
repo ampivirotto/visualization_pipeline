4edff96866512a7e706549f84deaf7e7ef0039cc
tuh00621 <68647867+tuh00621@users.noreply.github.com>
2021-02-26 14:37:37 -0500

    Update init.js

--- command_line/igv.js-flask/igvjs/static/js/init.js
+++ command_line/igv.js-flask/igvjs/static/js/init.js
@@ -6,17 +6,34 @@ function initBrowser() {
 
   div = document.getElementById("myDiv");
   options = {
-      genome: "hg19",
+      genome: {
+    "id":"canFam2",
+    "name": "canFam2",
+    "fastaURL": "static/data/public/canFam2.fa",
+    "indexURL": "static/data/public/canFam2.fa.fai",
+    
+    "tracks": [
+      {
+        "name": "Refseq Genes",
+        "url": "https://s3.amazonaws.com/igv.org.genomes/hg38/refGene.txt.gz",
+        "order": 1000000,
+        "indexed": false
+      }
+    ]
+  },
       //locus: "22:24,375,771-24,376,878",
       tracks: [
                   {
-                      url: 'static/data/public/test.bw',
-                      name: 'BigWig'
-                  },
-                  {
-                      url: 'static/data/public/test.bedGraph',
-                      name: 'bedgraph'
-                  }
+                    type: "variant",
+                    format: "vcf",
+                    url: "static/data/public/test.vcf.gz",
+                    indexURL: "static/data/public/test.vcf.gz.tbi",
+                    name: "test.vcf",
+                    squishedCallHeight: 1,
+                    expandedCallHeight: 10,
+                    displayMode: "expanded",
+                    visibilityWindow: 1000000000
+                    }
               ]
   };
 
